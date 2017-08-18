6c88ce20a58c23ca2dcc9cbd611661b32d2e1468
nirvanarsx <nirvana.lisscreative@gmail.com>
2017-08-18 11:03:22 +0300

    complete

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use AppBundle\Entity\Shortener;
@@ -18,15 +19,15 @@ class DefaultController extends Controller
 
         $form = $this->createFormBuilder($current_url)
             ->add('url', TextType::class)
+            ->add('shortUrl', TextType::class,[
+                'required' => false
+            ])
             ->getForm();
         $form->handleRequest($request);
 
         if ($form->isValid() && $form->isSubmitted()) {
 
             $data = $form->getData();
-//            dump($data);
-//            dump($data->getUrl());
-//            die();
 
             $exist_url = $this->getDoctrine()->getRepository('AppBundle:Shortener')->findOneBy([
                'url' => $data->getUrl()
@@ -37,7 +38,9 @@ class DefaultController extends Controller
                 $em->persist($current_url);
                 $em->flush();
 
-                $current_url->setShortUrl(base64_encode($current_url->getId()).'.com');
+                if ($current_url->getShortUrl() == null) {
+                    $current_url->setShortUrl(base64_encode($current_url->getId()).'.com');
+                }
 
                 $em->flush();
             }
@@ -50,9 +53,17 @@ class DefaultController extends Controller
             ]);
         }
 
-        // replace this example code with whatever you need
         return $this->render('AppBundle:main:index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+
+
+    public function showAction($shortUrl)
+    {
+        $url = $this->getDoctrine()->getRepository('AppBundle:Shortener')->findOneByShortUrl([$shortUrl])->getUrl();
+
+        return new RedirectResponse($url);
     }
 }
